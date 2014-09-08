ef4f38ce91699343fd0d7be40790c908515709e1
Robin Scheibler <fakufaku@gmail.com>
2014-09-08 18:26:22 +0200

    First commit

---
+++ lstsq_td.py
@@ -0,0 +1,134 @@
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.linalg import toeplitz
+from scipy.io import wavfile
+from scipy.signal import resample,fftconvolve
+
+import pyroomacoustics as pra
+
+# Beam pattern figure properties
+freq=[800, 1600]
+figsize=(1.88,2.24)
+xlim=[-4,8]
+ylim=[-4.9,9.4]
+
+# Some simulation parameters
+Fs = 8000
+t0 = 1./(Fs*np.pi*1e-2)  # starting time function of sinc decay in RIR response
+absorption = 0.90
+max_order_sim = 1
+sigma2_n = 1e-7
+
+# Room 1 : Shoe box
+room_dim = [4, 6]
+
+# the good source is fixed for all 
+good_source = [1, 4.5]       # good source
+normal_interferer = [2.8, 4.3]   # interferer
+hard_interferer = [1.5, 3]   # interferer in direct path
+#normal_interferer = hard_interferer
+
+# microphone array design parameters
+mic1 = [2, 1.5]         # position
+M = 8                    # number of microphones
+d = 0.08                # distance between microphones
+phi = 0.                # angle from horizontal
+max_order_design = 1    # maximum image generation used in design
+shape = 'Linear'        # array shape
+
+# define the array processing type
+N = 2048                # FFT length
+hop = 512              # hop between frames
+zp = 512               # zero padding (same front and back)
+
+# TD filter length
+Lg_t = 0.05 # Filter size in seconds
+Lg = int(np.ceil(Lg_t*Fs))
+Lgp = np.floor(0.4*Lg)
+Lgm = Lg - Lgp
+print 'Lg=',Lg
+
+# create a microphone array
+if shape is 'Circular':
+    R = circular2DArray(mic1, M, phi, d*M/(2*np.pi)) 
+else:
+    R = pra.linear2DArray(mic1, M, phi, d) 
+mics = pra.Beamformer(R, Fs, N, Lg=Lg, hop=hop, zpf=zp, zpb=zp)
+
+# The first signal (of interest) is singing
+rate1, signal1 = wavfile.read('samples/singing_'+str(Fs)+'.wav')
+signal1 = np.array(signal1, dtype=float)
+signal1 = pra.normalize(signal1)
+signal1 = pra.highpass(signal1, Fs)
+delay1 = 0.
+
+# the second signal (interferer) is some german speech
+rate2, signal2 = wavfile.read('samples/german_speech_'+str(Fs)+'.wav')
+signal2 = np.array(signal2, dtype=float)
+signal2 = pra.normalize(signal2)
+signal2 = pra.highpass(signal2, Fs)
+delay2 = 1.
+
+# create the room with sources and mics
+room1 = pra.Room.shoeBox2D(
+    [0,0],
+    room_dim,
+    Fs,
+    t0 = t0,
+    max_order=max_order_sim,
+    absorption=absorption,
+    sigma2_awgn=sigma2_n)
+
+# add mic and good source to room
+room1.addSource(good_source, signal=signal1, delay=delay1)
+room1.addMicrophoneArray(mics)
+
+# add interferer
+room1.addSource(normal_interferer, signal=signal2, delay=delay2)
+
+# simulate the acoustic
+room1.compute_RIR()
+room1.simulate()
+
+# Compute the beamforming weights depending on room geometry
+good_sources = room1.sources[0].getImages(max_order=max_order_design)
+bad_sources = room1.sources[1].getImages(max_order=max_order_design)
+mics.rakeMaxSINRWeights(good_sources, bad_sources, 
+                        R_n = sigma2_n*np.eye(mics.M), 
+                        rcond=0., 
+                        attn=True, ff=False)
+
+# project weights onto short filters, allocate 60% of taps for non-causal part
+mics.filtersFromWeights(non_causal=0.7)
+mics.weightsFromFilters()
+
+# save to output file
+output = mics.process(FD=True)
+inp = pra.highpass(pra.normalize(mics.signals[mics.M/2]), Fs)
+out = pra.highpass(pra.normalize(output), Fs)
+
+wavfile.write('output_samples/input.wav', Fs, inp)
+wavfile.write('output_samples/output.wav', Fs, out)
+
+'''
+Plot Stuff
+'''
+# plot the room and beamformer
+room1.plot(img_order=np.minimum(room1.max_order, 1), 
+        freq=freq)
+
+# plot the beamforming weights
+plt.figure()
+mics.plot()
+
+# plot angle/frequency beampatterns
+plt.figure()
+mics.plot_beam_response()
+
+# plot before/after processing
+plt.figure()
+pra.comparePlot(inp, out, Fs)
+
+plt.show()
